26d2203ead75a15b7edca155cb966993293198e3
daimajie <daimajie@qq.com>
2018-05-09 17:48:46 +0800

    first commit

---
+++ static/js/data.js
@@ -0,0 +1,46 @@
+var data = [
+	{
+		name : 'components',
+		cite : '组件',
+		icon : 'layui-icon layui-icon-component',
+		childs : [
+			{
+				name : 'one',
+				href : 'one.html',
+				cite : '第一页',
+			},
+			{
+				name : 'two',
+				href : 'two.html',
+				cite : '第二页',
+			},
+			{
+				name : 'three',
+				href : 'three.html',
+				cite : '第三页',
+			},
+			{
+				name : 'four',
+				href : 'four.html',
+				cite : '第四页',
+			},
+			{
+				name : 'five',
+				href : 'five.html',
+				cite : '第五页',
+			},
+			{
+				name : 'six',
+				href : 'six.html',
+				cite : '第六页',
+			},
+			
+		]
+	},
+	{
+		name : 'get',
+		href : 'get.html',
+		icon : 'layui-icon layui-icon-auz',
+		cite : '授权',
+	}
+];
